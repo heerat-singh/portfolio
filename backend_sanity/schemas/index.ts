c0d4ac497a2eecb01ca83188ff38138b1ac2fc96
heerat-singh <heeratsingh54@gmail.com>
2023-10-21 17:32:47 -0400

    backend setup completed - frontend started

---
+++ backend_sanity/schemas/index.ts
@@ -0,0 +1,17 @@
+import testimonials from './testimonials'
+
+import about from './about'
+
+import brands from './brands'
+
+import contact from './contact'
+
+import experiences from './experiences'
+
+import skills from './skills'
+
+import workExperience from './workExperience'
+
+import works from './works'
+
+export const schemaTypes = [testimonials,about,brands,contact,experiences,workExperience,works,skills]
