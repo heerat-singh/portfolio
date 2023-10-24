0c85dbde3c08d029e2b8e53aeb7c866c4e7220a7
heerat-singh <heeratsingh54@gmail.com>
2023-10-23 21:31:41 -0400

    security keys in env

--- portfolio-frontend/src/client.js
+++ portfolio-frontend/src/client.js
@@ -2,11 +2,11 @@ import {createClient} from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
 
 export const client = createClient({
-    projectId: 'nu78uh01',
+    projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
     dataset: 'production',
     apiVersion: '2023-10-23',
     useCdn: true,
-    token: 'skiUq2R5BLM62nN3g6TlCVwPbCGyvYceBIXsVT28NRYnbBNd3wjGkfkBC4wrf3N94MFufFInzuHVEx0HttHe6DCgghSnvWil19kKVdB7cjhte1Tm0uwqnN0TxPixh8AjtdvX7vr8GBVHu72m3LZ4OMBQze1ChG8MZ7n9VECTzVsEnmcRMYsX'
+    token: process.env.REACT_APP_SANITY_TOKEN
 });
 
 const builder = imageUrlBuilder(client);
